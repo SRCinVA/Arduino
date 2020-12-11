2e0e32d845214efc6b4544356ed320b769274f94
SRCinVA <scitarella@hotmail.com>
2020-12-10 20:00:50 -0500

    introduced variables_tutorial_4
    C++ in VS Code has issues with declaring string variables.

--- introducing_variables_4.cpp
+++ introducing_variables_4.cpp
@@ -1,46 +1,59 @@
+//variables are defined above the setup step
+
+int redLED=8;
+float Pi=3.14;
+int dit=250; //short blips
+int dah=700; //long blips
+int longW=2000;
+string myName="Steve";
+
 void setup() {
-pinMode(13,OUTPUT);
+pinMode(redLED,OUTPUT);
 }
 
 void loop() {
-digitalWrite(13,HIGH);
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH); 
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH); 
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+digitalWrite(redLED, HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+digitalWrite(redLED, HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+delay(longW);
 
-digitalWrite(13, HIGH); 
-delay(1000);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH); 
-delay(1000);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH);
- delay(1000);
-digitalWrite(13, LOW);
-delay(100);
 
-digitalWrite(13, HIGH);
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH);
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
-digitalWrite(13, HIGH);
-delay(50);
-digitalWrite(13, LOW);
-delay(100);
+digitalWrite(redLED, HIGH);
+delay(dah);
+digitalWrite(redLED, LOW);
+delay(dah);
+digitalWrite(redLED, HIGH);
+delay(dah);
+digitalWrite(redLED, LOW);
+delay(dah);
+digitalWrite(redLED, HIGH);
+delay(dah);
+digitalWrite(redLED, LOW);
+delay(dah);
+delay(longW);
+
+digitalWrite(redLED, HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+digitalWrite(redLED, HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+digitalWrite(redLED, HIGH);
+delay(dit);
+digitalWrite(redLED, LOW);
+delay(dit);
+delay(longW);
 }
 
 //note that it doesn't always have to be pin 13, but
