0d437aaebc783b47c85ff8abac2f3bf592e3f681
SRCinVA <scitarella@hotmail.com>
2020-12-13 20:56:40 -0500

    finished analog write (tutorial 7)

--- analog_write_tutorial_7.cpp
+++ analog_write_tutorial_7.cpp
@@ -1,4 +1,5 @@
 int redPin=9;
+int bright=255 // in analog, you can grade things between 0 and 256 (inclusive to 255)
 
 void setup() {
 
@@ -8,6 +9,6 @@ pinMode(redPin,OUTPUT);
 
 void loop () {
 
-analogWrite(redPin,)
-
+analogWrite(redPin,bright);
+//you could introduce dealys and multiple inputs to see it change.
 }
