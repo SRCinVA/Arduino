3cc6619a6f156f0a65f84275f333dcfa68f50dc9
SRCinVA <scitarella@hotmail.com>
2020-12-14 21:40:01 -0500

    covered pulse width modulation

--- analog_write_tutorial_7.cpp
+++ analog_write_tutorial_7.cpp
@@ -12,3 +12,9 @@ void loop () {
 analogWrite(redPin,bright);
 //you could introduce dealys and multiple inputs to see it change.
 }
+
+// on the oscilliscope, if you entered 127 brightness, 
+// it would swing between 0 and 5 in a sine-wave function.
+// to average out to 2.5 volts.
+// This is called pulse-width modulation.
+// Capacitors smooth over those stark swings pretty well.
