fe292604b39a0a7842d4d8336451fc2c16dd81f3
SRCinVA <scitarella@hotmail.com>
2020-12-23 20:51:34 -0500

    if statements tutorial 13

--- if_statements_tutorial_13.cpp
+++ if_statements_tutorial_13.cpp
@@ -2,11 +2,13 @@ int myPin=A2;
 int readVal; // will just read it; only need to declare it. For voltage.
 float V2; // same as above; also for voltage.
 int dt=250;
+int redPin=9;
 
 void setup() {
 
     Serial.begin(9600);
-
+    pinMode(myPin,INPUT);
+    pinMode(myPin,OUTPUT);
 }
 
 void loop() {
@@ -15,7 +17,22 @@ void loop() {
     V2=(5./1023.)*readVal;
     Serial.print("Potentiometer Voltage is: ");
     Serial.println(V2);
+    if (V2==5.0){ //turns on when it's 5.
+        digitalWrite(redPin,HIGH);
+    }
+    if (V2!=5.0) { //turns off at any other voltage.
+        digitalWrite(redPin,LOW);
+    }
+
+    //if(V2>2 && V2<3){
+        // digitalWrite(redPin,HIGH)
+        //}
+    //if (V2<2 || V2>3){
+        // digitalWrite(redPin, LOW)
+        //}
+
     delay(dt);
 }
 
 // if the Serial monitor is open, the program won't download. Close it and then try again.
+// && for 'and' and || for 'or'
