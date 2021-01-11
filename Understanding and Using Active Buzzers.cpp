637c39ccc4dcfeab20a7c43e4258eed7e8e58489
SRCinVA <scitarella@hotmail.com>
2021-01-10 20:35:32 -0500

    Further modifications to the code

--- Understanding and Using Active Buzzers.cpp
+++ Understanding and Using Active Buzzers.cpp
@@ -1,29 +1,33 @@
 // Understanding and Using Active Buzzers
 
-int myNum;
+int potVal;
 int buzzPin = 8;
-String msg="Please input your number";
-int dt = 1000;
+int potPin = A3;
+//String msg="Please input your number";
+//int dt = 1000;
 
 void setup() {
 
 Serial.begin(9600);
 pinMode(buzzPin, OUTPUT);
+pinMode(potPin, INPUT);
 
 }
-
 
 void loop() {
 
-Serial.println(msg);
-while (Serial.available) == 0 {
+//Serial.println(msg);
+//while (Serial.available) == 0 {
 
-}
-myNum = Serial.parseInt();
-if (myNum > 10) {
+//}
+potVal = analogRead(potPin);
+if (potVal > 1000) {
     digitalWrite(buzzPin, HIGH);
-    delay(dt);
-    digitalWrite(buzzPin, LOW);
+    //delay(dt);
+    //digitalWrite(buzzPin, LOW);
+    potVal = analogRead(potPin); //you need to get it to read again
+    Serial.println(potVal);
+    //there might be some jitter sounds right around 1000.
 }
-
+digitalWrite(buzzPin,LOW); //when it goes below 1000, it will exit the loop and end here.
 }
