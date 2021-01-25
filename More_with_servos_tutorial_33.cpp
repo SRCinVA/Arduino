1747c49d9fd4073124aaf96a58dca4b7899723fa
SRCinVA <scitarella@hotmail.com>
2021-01-25 13:58:15 -0500

    Finished it.

--- More_with_servos_tutorial_33.cpp
+++ More_with_servos_tutorial_33.cpp
@@ -1,4 +1,7 @@
 // joysticks are far more precise controls than knobs.
+#include <Servo.h> //need to import the library
+Servo Xservo;
+Servo Yservo;
 
 int Xpin=A0;
 int Ypin=A1;
@@ -12,6 +15,7 @@ int Xval; // to read off of
 int Yval; // to read off of
 int Sval;
 int dt=200;
+
 void setup(){
 
 Serial.begin(9600);
@@ -21,25 +25,39 @@ pinMode(Spin,INPUT);
 pinMode(XSpin,OUTPUT);
 pinMode(YSpin,OUTPUT);
 
-digitalWrite(Spin, HIGH) // to power the switch function?
+Xservo.attach(XSpin);
+Yservo.attach(YSpin);
+
+digitalWrite(Spin, HIGH); // to power the switch function?
 
 }
 
-
 void loop(){
 
-Xval=analogRead(Xpin);
-WVx=(180./1023.)* Xval;
-Yval=analogRead(Ypin);
-WVy=(180. / 1023.) * Yval;
+Xval = analogRead(Xpin);
+WVx = (180./1023.) * Xval;
+Yval = analogRead(Ypin); 
+WVy = (180./1023.) * Yval;
 
 Sval=digitalRead(Spin);
+// now that you've calculated those values, you can give
+// those instructions to the servo.
+
+Xservo.write(WVx);
+Yservo.write(WVy);
+if (Sval==0){
+    digitalWrite(buzzPin,HIGH);
+}
+else {
+    digitalWrite(buzzPin,LOW);
+}
+
 delay(dt);
 Serial.print("X Value = ");
 Serial.print(Xval);
-Serial.print(" Y Value = ");
+Serial.print("Y Value = ");
 Serial.print(Yval);
-Serial.print(" Switch state is: ");
+Serial.print("Switch state is: ");
 Serial.println(Sval);
 
 }
