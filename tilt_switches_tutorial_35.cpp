01b3209d1e4150ec8cc673fb55c6d0a280a1674b
SRCinVA <scitarella@hotmail.com>
2021-01-29 16:15:58 -0500

    lesson complete

---
+++ tilt_switches_tutorial_35.cpp
@@ -0,0 +1,30 @@
+int tiltPin = 2;
+int tiltVal;
+int redPin=7;
+int greenPin=6;
+
+void setup() {
+pinMode(tiltPin,INPUT);
+pinMode(redPin,OUTPUT);
+pinMode(greenPin,OUTPUT);
+digitalWrite(tiltPin,HIGH); // you have to power the pullup resistor; that's why it's HIGH
+Serial.begin(9600);
+
+}
+
+void loop() {
+tiltVal=digitalRead(tiltPin);
+Serial.println(tiltVal); // if it is tilted, we should see a 1.
+
+if (tiltVal==0) {
+    digitalWrite(greenPin,HIGH);
+    digitalWrite(redPin,LOW);
+}
+if (tiltVal==1){
+    digitalWrite(greenPin,LOW);
+    digitalWrite(redPin,HIGH);
+}
+
+
+
+}
