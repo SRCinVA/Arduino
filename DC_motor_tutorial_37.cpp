13775332c93a94256a06fa38704a7cde86757509
SRCinVA <scitarella@hotmail.com>
2021-01-31 20:54:27 -0500

    Lesson finished

--- DC_motor_tutorial_37.cpp
+++ DC_motor_tutorial_37.cpp
@@ -2,7 +2,11 @@
 int speedPin=5;
 int dir1=4;
 int dir2=3;
-int mSpeed=255; //between 0 and 255
+int mSpeed=100; //between 0 and 255
+                // it will run at 100,
+                // but you need to give
+                // it a push
+
 
 
 void setup() {
@@ -13,7 +17,17 @@ Serial.begin(9600);
 }
 
 void loop() {
-digitalWrite(dir1,HIGH);
-digitalWrite(dir2,LOW);
-    
+digitalWrite(dir1,LOW);
+digitalWrite(dir2,HIGH);
+analogWrite (speedPin,255); // to give it a kickstart
+delay(100);
+analogWrite(speedPin,mSpeed); // with the kick, it will sustain at 100
+delay(5000);
+/* to make it go counterclockwise:
+
+digitalWrite(dir1, HIGH);
+digitalWrite(dir2, LOW);
+analogWrite(speedPin, mSpeed);
+
+*/
 }
