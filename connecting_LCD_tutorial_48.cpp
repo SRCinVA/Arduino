be8f4a1b96e06d6b526710e24867f21d2ddb44c2
SRCinVA <scitarella@hotmail.com>
2021-02-20 14:51:18 -0500

    Fun with for loops and clear()

--- connecting_LCD_tutorial_48.cpp
+++ connecting_LCD_tutorial_48.cpp
@@ -15,16 +15,24 @@ lcd.begin(16,2);  //it has 16 columns and 2 rows
 
 void loop() {
 lcd.setCursor(0,0);
-lcd.println("Hello World");
-lcd.setCursor(0,1); // this moves it down to the second line
+lcd.print("Watch me count");
+lcd.setCursor(0,1); // this moves the coming string down to the second line
+
+/*
 lcd.print("What's your name?");
 delay(1000); // hard-coded ...?!?!?
 lcd.clear() // if you don't, the entire field won't clear away
-
-/*
-
-
 */
 
+for(int j=1; j<=10; j=j+1){
+    lcd.setCursor(0,1); //this resets the cursor, so that the digits present one at a time
+    lcd.print(j);
+    delay(500);
+    //lcd.clear(); if you keep clear() in the for loop, it clears out "Watch me count" right after "1" presents.
+    // With clear() in the for loop, the "Watch me count" accidentally gets swept up in that first clear() execution.
+    // The rest of the number displays and clears happen as expected.
+
+}
+lcd.clear() // Sitting out here, "Watch me count" displays, then all the numbers, then everything completely refreshes
 
 }
