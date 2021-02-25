9c5d8ba46fa24dadb5b2a7e5a1312d0f2a738d21
SRCinVA <scitarella@hotmail.com>
2021-02-24 20:23:54 -0500

    last commit
    Cleaned up a few things. Float data types seemed to have any issue in VS Code, too.

--- calculator_withan_an_LCD_tutorial_49.cpp
+++ calculator_withan_an_LCD_tutorial_49.cpp
@@ -50,8 +50,11 @@ secNum = Serial.parseFloat();
 lcd.clear();
 lcd.setCursor(0,0);
 // ask
-lcd.print("Input operator (+,-,*,/)")
+lcd.print("Input (+,-,*,/)")
 // read
+while (Serial.available() == 0)
+{
+}
 op = Serial.readString();
 
 if (op=="+"){
@@ -76,25 +79,8 @@ lcd.print(op);
 lcd.print(secNum);
 lcd.print(" = ");
 lcd.print(answer);
-
-lcd.print("Watch me count");
-lcd.setCursor(0,1); // this moves the coming string down to the second line
-
-/*
-lcd.print("What's your name?");
-delay(1000); // hard-coded ...?!?!?
-lcd.clear() // if you don't, the entire field won't clear away
-*/
-
-for(int j=1; j<=10; j=j+1){
-    lcd.setCursor(0,1); //this resets the cursor, so that the digits present one at a time
-    lcd.print(j);
-    delay(500);
-    //lcd.clear(); if you keep clear() in the for loop, it clears out "Watch me count" right after "1" presents.
-    // With clear() in the for loop, the "Watch me count" accidentally gets swept up in that first clear() execution.
-    // The rest of the number displays and clears happen as expected.
-
-}
-lcd.clear() // Sitting out here, "Watch me count" displays, then all the numbers, then everything completely refreshes
-
+lcd.setCursor(0,1);
+lcd.print("Thank you!");
+delay(5000);
+lcd.clear();
 }
