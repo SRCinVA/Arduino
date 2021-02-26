374e532c956d579c5c0f50c129d850345c88c480
SRCinVA <scitarella@hotmail.com>
2021-02-25 20:54:18 -0500

    First commit

---
+++ Connect_and_use_DHT11_sensor_tutorial_50.cpp
@@ -0,0 +1,18 @@
+#include <DHT.h>
+#define Type DHT11  // sets up a constant
+int sensePin=2;
+DHT HT(sensePin,Type);   // this names the DHT object, where it's connected, and what type
+float Humidity;
+float TempC;
+float TempF;
+int setTime = 500;
+
+void setup() {
+Serial.begin(9600);
+HT.begin();   // does not take arguments
+delay(setTime) // good general practice gives things time to set up
+}
+
+void loop() {
+humidity=HT.readHumidity();  
+}
