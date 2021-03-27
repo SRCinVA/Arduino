b3d10e37d9db8321b68bf3bb1941f75d209614da
SRCinVA <scitarella@hotmail.com>
2021-03-26 22:07:44 -0400

    next commit

--- HC-SR04_tutorial_53.cpp
+++ HC-SR04_tutorial_53.cpp
@@ -6,11 +6,14 @@ void setup() {
 
 pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);
+Serial.begin(9600);
 }
 
 void loop() { // we need to go LOW -> HIGH -> LOW for a clear signal
-digitalWrite(trigPin,LOW);
+digitalWrite(trigPin,LOW); // start LOW
 delayMicroseconds(10);
-digitalWrite(trigPin, HIGH);
-
+digitalWrite(trigPin, HIGH); // stay HIGH until ...
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW); // ... the ping comes back, then goes LOW again
+delay(25);
 }
