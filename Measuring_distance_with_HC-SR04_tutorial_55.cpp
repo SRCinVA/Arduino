3cc71d92d64b8e1746d5b8cd90ce90a837d2038e
SRCinVA <scitarella@hotmail.com>
2021-04-23 16:57:27 -0400

    small changes (now finished)

--- Measuring_distance_with_HC-SR04_tutorial_55.cpp
+++ Measuring_distance_with_HC-SR04_tutorial_55.cpp
@@ -3,7 +3,7 @@ int echoPin = 11;
 float pingTravelTime; // we'll be reading this one
 float  pingTravelDistance;
 float distanceToTarget;
-
+int dt = 500;
 void setup() {
 
 pinMode(trigPin, OUTPUT);
@@ -17,11 +17,14 @@ delayMicroseconds(10);
 digitalWrite(trigPin, HIGH); // stay HIGH until ...
 delayMicroseconds(10);
 digitalWrite(trigPin, LOW); // ... the ping comes back, then goes LOW again
-pingTravelTime=pulseIn(echoPin, HIGH); // this measures the legnth of the HIGH pulse (for assessing distance?)
+pingTravelTime=pulseIn(echoPin, HIGH); // this measures the length of the HIGH pulse (for assessing distance?)
                     // we're looking for a HIGH pulse from the echo pin.
                     // we'd no longer need an oscilloscope
 delay(25);
 pingTravelDistance = (pingTravelTime * 765. * 5280 * 12.)/(3600. * 1000000);
 distanceToTarget = pingTravelDistance/2;
+Serial.print ("Your Distance to Target is: ");
 Serial.print(distanceToTarget);
+Serial.println(" inches");
+delay(dt);
 }
