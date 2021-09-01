b4d5794e0386cf6ef9f48965c559c8ecd7c186ed
SRCinVA <scitarella@hotmail.com>
2021-08-31 21:05:49 -0400

    He got it to work (it seems)

--- Attaching_an_Infrared_Remote_64.cpp
+++ Attaching_an_Infrared_Remote_64.cpp
@@ -6,14 +6,15 @@ decode_results cmd;  // command from the library (not the Arduino), where it rea
 void setup()
 {
 Serial.begin(9600)
-IR.enableIRIn();     // strange capitalization 
+IR.enableIRIn();     // strange capitalization ... 
 }
 
-void loop() {
-    while (IR.decode(&cmd)==0){
-
+void loop() {  // "we sit and wait for data"
+    while (IR.decode(&cmd) == 0){  // the ampersand gets the value and sends it to cmd (which is a variable created above)
+      // while it is empty ("0"), it will just turn over in an empty loop.
     }
+    // after someone presses a button:
     Serial.println(cmd.value, HEX);
-    delay(1500);
-    IR.resume();
+    delay(1500); // need to give it a delay so that it doesn't go to fast
+    IR.resume(); // this restarts the reading
 }
