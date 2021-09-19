e8f3b1f213797fd9fe54c6112c5b77f0918bc831
SRCinVA <scitarella@hotmail.com>
2021-09-18 21:34:25 -0400

    Got started

---
+++ Remote_Control_RGB_LED_with_Brightness_and_Color_Control_68.cpp
@@ -0,0 +1,141 @@
+#include <IRremote.h>  // need to import the library (from online).
+int IRpin = 9;
+IRrecv IR(IRpin);    //need to create the object and where it's connected to.
+decode_results cmd;  // command from the library (not the Arduino), where it reads input from the module (the remote)
+String myCom; 
+
+int rPin = 6;
+int gPin = 10;
+int bPin = 11;
+
+int rBright=255; // looks like maximum brightness for all.
+int gBright=255;
+int bBright=255;
+
+void setup()
+{
+Serial.begin(9600)
+IR.enableIRIn();     // strange capitalization ... 
+
+pinMode (rBright,OUTPUT);
+pinMode (gBright,OUTPUT);
+
+}
+
+
+
+void loop() {  // "we sit and wait for data"
+    while (IR.decode(&cmd) == 0){  // the ampersand gets the value and sends it to cmd (which is a variable created above)
+      // while it is empty ("0"), it will just turn over in an empty loop.
+    }
+    // after someone presses a button:
+    //Serial.println(cmd.value, HEX); // we told it that the value returned would be cmd, in the form of a hexidecimal.
+    // we don't actually need this, so later in the lesson, he took it away.
+    delay(1500); // need to give it a delay so that it doesn't go to fast
+    IR.resume(); // this restarts the reading
+    //Serial.println(cmd.value, HEX); // to show you the hex that cmd produces, but you don't need this
+    
+    if (cmd.value == 0xFF6897) { //or whatever hex value the remote produces ("0x" says that a hex is coming)
+        myCom = "zero";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF30CF) { 
+        myCom = "one";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF18E7) { 
+        myCom = "two";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF7A85) {
+        myCom = "three";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF10EF) {
+        myCom = "four";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF38C7) {
+        myCom = "five";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF5AA5) {
+        myCom = "six";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF42BD) {
+        myCom = "seven";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF4AB5) {
+        myCom = "eight";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF52AD) {
+        myCom = "nine";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF629D) {
+        myCom = "v+";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFFE21D) {
+        myCom = "fun";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF22DD) {
+        myCom = "rew";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF02FD) {
+        myCom = "play";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFFC23D) {
+        myCom = "ff";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFFE01F) {
+        myCom = "dn";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFFA857) {
+        myCom = "v-";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF906F) {
+        myCom = "up";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFF9867) {
+        myCom = "eq";
+        Serial.println(myCom);
+    }
+
+    if (cmd.value == 0xFFB04F) {
+        myCom = "st";
+        Serial.println(myCom);
+    }
+
+
+
+
+}
